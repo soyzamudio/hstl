464cd31c47b7f8c9ee06d5bb2c79e3446a723509
Jose Zamudio <zamudio@outlook.com>
2015-02-18 20:18:19 -0800

    Create Schemas

---
+++ server/models/apartment.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var apartmentSchema = mongoose.Schema({
+  name      : {type:String, requried:true},
+  bedrooms  : {type:Number, required:true},
+  rent      : {type:Number, required:true}
+  sqft      : {type:Number},
+  photo     : {type:String, required:true},
+  createdAt : {type: Date, default: Date.now, required: true},
+  pets      : [{type: mongoose.Schema.ObjectId, ref: 'Renter'}]
+});
+
+module.exports = mongoose.model('Apartment', apartmentSchema);
---
+++ server/models/renter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var renterSchema = mongoose.Schema({
+  name      : {type:String, requried:true},
+  age       : {type:Number, required:true},
+  gender    : {type:String, required:true}
+  photo     : {type:String, required:true},
+  createdAt : {type: Date, default: Date.now, required: true},
+});
+
+module.exports = mongoose.model('Renter', renterSchema);
