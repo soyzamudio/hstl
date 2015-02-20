51b9233f629a3f65071a8e386725fe68b89e34ef
Jose Zamudio <zamudio@outlook.com>
2015-02-19 17:11:03 -0800

    Add filters to search

--- server/routes/apartments/index.js
+++ server/routes/apartments/index.js
@@ -6,7 +6,27 @@ var _ = require('lodash');
 
 module.exports = {
   handler: function(request, reply) {
-    Apartment.find({}, function(err, apartments) {
+
+    Object.keys(request.query).forEach(function(key) {
+      if (!request.query[key]) {
+        delete request.query[key];
+      }
+    });
+
+    if (request.query.min) {
+      request.query.rent = { $gte: request.query.min };
+      delete request.query.min;
+    }
+
+    if (request.query.max) {
+      request.query.rent = request.query.rent || {};
+      _.merge(request.query.rent, { $lte: request.query.max });
+      delete request.query.max;
+    }
+
+    console.log('query', request.query);
+
+    Apartment.find(request.query, function(err, apartments) {
       reply.view('templates/apartments/index', {path: '/apartments', active: active, apartments: apartments, _:_});
     });
   }
